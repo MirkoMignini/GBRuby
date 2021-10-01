630f40fd6385b8188578d57b2cd31a06657ad18c
MirkoMignini <mirko.mignini@gmail.com>
2021-10-01 16:10:16 +0200

    Added sei to bits instructions

--- game_boy/cpu/instructions/bits.rb
+++ game_boy/cpu/instructions/bits.rb
@@ -73,4 +73,78 @@ module Bits
   def bit_5_a_hl() = bit(5, read_byte(hl))
   def bit_6_a_hl() = bit(6, read_byte(hl))
   def bit_7_a_hl() = bit(7, read_byte(hl))
+
+  def set(index, value); end
+  def set_0_a_hl; write_byte(hl, read_byte(hl) | 1); end
+  def set_1_a_hl; write_byte(hl, read_byte(hl) | 2); end
+  def set_2_a_hl; write_byte(hl, read_byte(hl) | 4); end
+  def set_3_a_hl; write_byte(hl, read_byte(hl) | 8); end
+  def set_4_a_hl; write_byte(hl, read_byte(hl) | 16); end
+  def set_5_a_hl; write_byte(hl, read_byte(hl) | 32); end
+  def set_6_a_hl; write_byte(hl, read_byte(hl) | 64); end
+  def set_7_a_hl; write_byte(hl, read_byte(hl) | 128); end
+
+  def set_0_a; @a |= 1; end
+  def set_0_b; @b |= 1; end
+  def set_0_c; @c |= 1; end
+  def set_0_d; @d |= 1; end
+  def set_0_e; @e |= 1; end
+  def set_0_h; @h |= 1; end
+  def set_0_l; @l |= 1; end
+
+  def set_1_a; @a |= 2; end
+  def set_1_b; @b |= 2; end
+  def set_1_c; @c |= 2; end
+  def set_1_d; @d |= 2; end
+  def set_1_e; @e |= 2; end
+  def set_1_h; @h |= 2; end
+  def set_1_l; @l |= 2; end
+
+  def set_2_a; @a |= 4; end
+  def set_2_b; @b |= 4; end
+  def set_2_c; @c |= 4; end
+  def set_2_d; @d |= 4; end
+  def set_2_e; @e |= 4; end
+  def set_2_h; @h |= 4; end
+  def set_2_l; @l |= 4; end
+
+  def set_3_a; @a |= 8; end
+  def set_3_b; @b |= 8; end
+  def set_3_c; @c |= 8; end
+  def set_3_d; @d |= 8; end
+  def set_3_e; @e |= 8; end
+  def set_3_h; @h |= 8; end
+  def set_3_l; @l |= 8; end
+
+  def set_4_a; @a |= 16; end
+  def set_4_b; @b |= 16; end
+  def set_4_c; @c |= 16; end
+  def set_4_d; @d |= 16; end
+  def set_4_e; @e |= 16; end
+  def set_4_h; @h |= 16; end
+  def set_4_l; @l |= 16; end
+
+  def set_5_a; @a |= 32; end
+  def set_5_b; @b |= 32; end
+  def set_5_c; @c |= 32; end
+  def set_5_d; @d |= 32; end
+  def set_5_e; @e |= 32; end
+  def set_5_h; @h |= 32; end
+  def set_5_l; @l |= 32; end
+
+  def set_6_a; @a |= 64; end
+  def set_6_b; @b |= 64; end
+  def set_6_c; @c |= 64; end
+  def set_6_d; @d |= 64; end
+  def set_6_e; @e |= 64; end
+  def set_6_h; @h |= 64; end
+  def set_6_l; @l |= 64; end
+
+  def set_7_a; @a |= 128; end
+  def set_7_b; @b |= 128; end
+  def set_7_c; @c |= 128; end
+  def set_7_d; @d |= 128; end
+  def set_7_e; @e |= 128; end
+  def set_7_h; @h |= 128; end
+  def set_7_l; @l |= 128; end
 end

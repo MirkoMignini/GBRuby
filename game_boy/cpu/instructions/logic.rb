f54f252235a21376f2f30cc26c6b420eb5ca4f8c
MirkoMignini <mirko.mignini@gmail.com>
2021-10-05 17:53:33 +0200

    Better logic methods

--- game_boy/cpu/instructions/logic.rb
+++ game_boy/cpu/instructions/logic.rb
@@ -1,31 +1,34 @@
 module Logic
-  def and_a; @a &= @a; set_flags(@a == 0, 0, 1, 0); end
-  def and_b; @a &= @b; set_flags(@a == 0, 0, 1, 0); end
-  def and_c; @a &= @c; set_flags(@a == 0, 0, 1, 0); end
-  def and_d; @a &= @d; set_flags(@a == 0, 0, 1, 0); end
-  def and_e; @a &= @e; set_flags(@a == 0, 0, 1, 0); end
-  def and_h; @a &= @h; set_flags(@a == 0, 0, 1, 0); end
-  def and_l; @a &= @l; set_flags(@a == 0, 0, 1, 0); end
-  def and_d8; @a &= pc_read_byte; set_flags(@a == 0, 0, 1, 0); end
-  def and_a_hl; @a &= read_byte(hl); set_flags(@a == 0, 0, 1, 0); end
+  def _and(value); @a &= value; set_flags(@a == 0, 0, 1, 0); end
+  def and_a()= _and(@a)
+  def and_b()= _and(@b)
+  def and_c()= _and(@c)
+  def and_d()= _and(@d)
+  def and_e()= _and(@e)
+  def and_h()= _and(@h)
+  def and_l()= _and(@l)
+  def and_d8()= _and(pc_read_byte)
+  def and_a_hl()= _and(read_byte(hl))
 
-  def or_a; @a |= @a; set_flags(@a == 0, 0, 0, 0); end
-  def or_b; @a |= @b; set_flags(@a == 0, 0, 0, 0); end
-  def or_c; @a |= @c; set_flags(@a == 0, 0, 0, 0); end
-  def or_d; @a |= @d; set_flags(@a == 0, 0, 0, 0); end
-  def or_e; @a |= @e; set_flags(@a == 0, 0, 0, 0); end
-  def or_h; @a |= @h; set_flags(@a == 0, 0, 0, 0); end
-  def or_l; @a |= @l; set_flags(@a == 0, 0, 0, 0); end
-  def or_d8; @a |= pc_read_byte; set_flags(@a == 0, 0, 0, 0); end
-  def or_a_hl; @a |= read_byte(hl); set_flags(@a == 0, 0, 0, 0); end
+  def _or(value); @a |= value; set_flags(@a == 0, 0, 0, 0); end
+  def or_a()= _or(@a)
+  def or_b()= _or(@b)
+  def or_c()= _or(@c)
+  def or_d()= _or(@d)
+  def or_e()= _or(@e)
+  def or_h()= _or(@h)
+  def or_l()= _or(@l)
+  def or_d8()= _or(pc_read_byte)
+  def or_a_hl()= _or(read_byte(hl))
 
-  def xor_a; @a ^= @a; set_flags(@a == 0, 0, 0, 0); end
-  def xor_b; @a ^= @b; set_flags(@a == 0, 0, 0, 0); end
-  def xor_c; @a ^= @c; set_flags(@a == 0, 0, 0, 0); end
-  def xor_d; @a ^= @d; set_flags(@a == 0, 0, 0, 0); end
-  def xor_e; @a ^= @e; set_flags(@a == 0, 0, 0, 0); end
-  def xor_h; @a ^= @h; set_flags(@a == 0, 0, 0, 0); end
-  def xor_l; @a ^= @l; set_flags(@a == 0, 0, 0, 0); end
-  def xor_d8; @a ^= pc_read_byte; set_flags(@a == 0, 0, 0, 0); end
-  def xor_a_hl; @a ^= read_byte(hl); set_flags(@a == 0, 0, 0, 0); end
+  def xor(value); @a ^= value; set_flags(@a == 0, 0, 0, 0); end
+  def xor_a()= xor(@a)
+  def xor_b()= xor(@b)
+  def xor_c()= xor(@c)
+  def xor_d()= xor(@d)
+  def xor_e()= xor(@e)
+  def xor_h()= xor(@h)
+  def xor_l()= xor(@l)
+  def xor_d8()= xor(pc_read_byte)
+  def xor_a_hl()= xor(read_byte(hl))
 end

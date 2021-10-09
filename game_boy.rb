3002ff0d8312fba08ca78278abd905ae7f761d81
MirkoMignini <mirko.mignini@gmail.com>
2021-10-09 22:21:51 +0200

    Device exposes vram

--- game_boy.rb
+++ game_boy.rb
@@ -11,7 +11,7 @@ class GameBoy
 
   CYCLES_PER_FRAME = 70_224.freeze
 
-  attr_reader :cpu, :bus, :ppu, :cartridge, :timer, :io_registers, :hram
+  attr_reader :cpu, :bus, :ppu, :cartridge, :timer, :io_registers, :hram, :vram
 
   def initialize
     init_bus
